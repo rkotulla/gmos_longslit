caa60daef010da268c73c456f354bf1d8b0050b3
Ralf Kotulla <kotulla@wisc.edu>
2015-10-08 11:33:20 -0500

    Added new modules to handle spectra extraction and sky-subtraction.

---
+++ extract.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os, sys, numpy, pyfits
+
+if __name__ == "__main__":
+    
+    fitsfile = sys.argv[1]
+    ap = [int(d) for d in sys.argv[2].split(":")]
+
+    hdu = pyfits.open(fitsfile)
+
+    data = hdu['SCI'].data
+
+    cutout = data[ap[0]-1:ap[1], :]
+    
+    spec1d = numpy.mean(cutout, axis=0)
+
+    print spec1d.shape
+
+    idx = numpy.arange(spec1d.shape[0]) + 1
+    hdr = hdu['SCI'].header
+
+    wl = (idx - hdr['CRPIX1']) * hdr['CD1_1'] + hdr['CRVAL1']
+
+    try:
+        wlcorr = [float(d) for d in sys.argv[3].split("-")]
+        d_wl = wlcorr[1] - wlcorr[0]
+        wl += d_wl
+    except:
+        pass
+
+    try:
+        print "masking"
+        blocks = sys.argv[4].split(",")
+        for block in blocks:
+            b12 = [int(d) for d in block.split(":")]
+            if (b12[0] < 0):
+                spec1d[(-b12[0]-1):(-b12[1])] = numpy.NaN
+            else:
+                wl_mask = (wl > b12[0]) & (wl < b12[1])
+                spec1d[wl_mask] = numpy.NaN
+    except:
+        pass
+
+    comb = numpy.append(wl.reshape((-1,1)),
+                        spec1d.reshape((-1,1)), axis=1)
+    print comb.shape
+
+    outfile = fitsfile[:-5]+".spec"
+    with open(outfile, "w") as of:
+        numpy.savetxt(of, comb)
+        print >>of, "\n\n\n\n\n"
+
+    
---
+++ skysubtract.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os, sys, numpy, pyfits, scipy
+
+import scipy, scipy.ndimage
+
+
+def subsky(fitsfile):
+
+    hdu = pyfits.open(fitsfile)
+
+    data = hdu['SCI'].data
+
+    #
+    # Apply median filter in spatial direction
+    # this should get rid of the continuum without affecting the skylines
+    #
+    
+    
+    data_filtered = scipy.ndimage.filters.median_filter(
+        input=data, 
+        size=(1,75), 
+        footprint=None, 
+        output=None, 
+        mode='reflect', 
+        cval=0.0, 
+        origin=0)
+
+    skylines = hdu['SCI'].data - data_filtered
+
+    # now combine all skylines
+    skyspec = numpy.mean(skylines, axis=0)
+
+    print skylines.shape
+    print skyspec.shape
+
+    skyspec_2d = skyspec.reshape((1, -1))
+    print skyspec_2d.shape
+
+    contnorm = numpy.mean(data_filtered, axis=0).reshape((1, -1))
+    print contnorm.shape
+
+    #skysub = (hdu['SCI'].data - skyspec_2d) / contnorm
+    skysub = (hdu['SCI'].data - skyspec_2d)
+
+    hdu['SCI'].data = skysub
+
+
+    hdu.writeto(fitsfile[:-5]+".skysub2.fits", clobber=True)
+
+
+if __name__ == "__main__":
+
+    for fitsfile in sys.argv[1:]:
+        subsky(fitsfile)
+
+
+
+
+
+    
